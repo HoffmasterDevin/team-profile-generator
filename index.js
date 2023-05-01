3aa851d56abf3c1e8cede40d3ce451bddc9a9ec4
HoffmasterDevin <118146567+HoffmasterDevin@users.noreply.github.com>
2023-05-01 14:54:02 -0500

    added starting file structure

---
+++ index.js
@@ -0,0 +1,15 @@
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const path = require('path');
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+const teamMembers = [];
+
+
+const questions = [{
+
+
+}]
